7521c875d1caf38ec305671cc145ea6a6799ff14
Marco019816 <mtshimanga@oroafrica.com>
2023-04-21 15:10:02 +0200

    MVC demo with C#

---
+++ Pages/Checkout/ThanksYou.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PizzaSolution.Pages.Checkout
+{
+    public class ThanksYouModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
